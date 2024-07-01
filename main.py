1a13a4a31d67a9e8a9660d90b063b3a799faab7e
QuickAxe <106764539+ManselInfinity@users.noreply.github.com>
2024-07-01 17:27:32 +0530

    it works now, somewhat

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import cv2 as cv
 import numpy as np
+from math import floor
 
 vid = cv.VideoCapture(0)
 
 # resolution to represent each character by, in pixels
-asciiWidth = 8
+asciiWidth = 4
 
 # ascii array to store the mapping
 
-asciiArr = [" ", ".", ";", "c", "o", "P", "O", "?", "#", "█"]
+asciiArr = [' ', '.', '-', "'", ':', '=', '+', '!', 'r', 'c', '*', 'v', ')', 'J', '7',
+            '(', '|', 'F', 'i', '}', 'f', 'a', ']', '2', 'E', 'S', 'w', 'B', 'g', '0', '%', '&', '@']
 
 
 while (True):
@@ -42,13 +44,15 @@ while (True):
     resizedFrame = cv.resize(
         frame, (originalX // asciiWidth, originalX // asciiWidth))
 
+    resizedFrame = cv.GaussianBlur(resizedFrame, ksize=(3, 3), sigmaX=3)
+
     # ======================================================== quantize the image into bins =================================================================
 
     resizedFrame = cv.cvtColor(resizedFrame, cv.COLOR_BGR2GRAY)
 
-    resizedFrame = resizedFrame % 10
+    resizedFrame = resizedFrame // len(asciiArr)
 
-    asciiFrame = np.zeros((originalX, originalX, 1), dtype=np.uint8)
+    asciiFrame = np.zeros((originalX, originalX), dtype=np.uint8)
 
     # ======================================================== assign respective ascii values to the frame ==================================================
     # using nested loops, which is a horrible way to do it
@@ -67,17 +71,20 @@ while (True):
     height_in_pixels = 4  # or 20, code works either way
     fontScale = (height_in_pixels - thickness) / factor
 
-    for i in range(0, originalX - 1, asciiWidth):
-        for j in range(0, originalX - 1, asciiWidth):
+    for i in range(0, originalX, asciiWidth):
+        for j in range(0, originalX , asciiWidth):
 
             pixel = resizedFrame[i // asciiWidth, j // asciiWidth]
 
-            asciiChar = asciiArr[pixel]
+            asciiChar = asciiArr[pixel % len(asciiArr)]
 
-            cv.putText(asciiFrame, asciiChar, (i, j), fontFace=fontFace,
-                       fontScale=fontScale, color=255, thickness=thickness, bottomLeftOrigin=False)
+            asciiFrame = cv.putText(asciiFrame, asciiChar, (i, j), fontFace=fontFace,
+                                    fontScale=fontScale, color=255, thickness=thickness, bottomLeftOrigin=False)
+
+    asciiFrame = cv.rotate(asciiFrame, cv.ROTATE_90_CLOCKWISE)
 
     cv.imshow("art", asciiFrame)
+    cv.imshow("original", resizedFrame * 20)
 
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
