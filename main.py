92a1dcac203b099f8ef8d43bd4725980027a986c
QuickAxe <106764539+ManselInfinity@users.noreply.github.com>
2024-07-01 20:18:00 +0530

    it works rather well now

--- main.py
+++ main.py
@@ -8,17 +8,15 @@ vid = cv.VideoCapture(0)
 asciiWidth = 4
 
 # ascii array to store the mapping
-
-asciiArr = [" ", '.', '-', "'", ':', '_', ',', '^', '=', ';', '>', '<', '+', '!', 'r', 'c', '*', '/', 'z', '?', 's', 'L', 'T', 'v', ')', 'J', '7',
-            '(', '|', 'F', 'i', '{', 'C', '}', 'f', 'I', '3', '1', 't', 'l', 'u', '[', 'n', 'e', 'o', 'Z', '5', 'Y', 'x', 'j', 'y', 'a', ']', '2', 'E', 'S', 'w', 'q', 'k', 'P', '6', 'h', '9', 'd', '4', 'V', 'p', 'O', 'G', 'b', 'U', 'A', 'K', 'X', 'H', 'm', '8', 'R', 'D', '#', '$', 'B', 'g', '0', 'M', 'N', 'W', 'Q', '%', '&', '@']
-
+asciiArr = [' ', '.', '-', "'", ':', '_', ',', '^', '=', ';', '>', '<', '+', '!', 'r', 'c', '*', '/', 'z', '?', 's', 'L', 'T', 'v', ')', 'J', '7',
+            '(', '|', 'F', 'i', '{', 'C', '}', 'f', 'I', '3', '1', 't', 'l', 'u', '[', 'n', 'e', 'o', 'Z', '5', 'Y', 'x', 'j', 'y', 'a', ']', '2',
+            'E', 'S', 'w', 'q', 'k', 'P', '6', 'h', '9', 'd', '4', 'V', 'p', 'O', 'G', 'b', 'U', 'A', 'K', 'X', 'H', 'm', '8', 'R', 'D', '#', '$',
+            'B', 'g', '0', 'M', 'N', 'W', 'Q', '%', '&', '@']
 
 while (True):
 
     ret, frame = vid.read()
     originalX, originalY, _ = frame.shape
-
-    orininalDims = (originalX, originalY)
 
     # ===========================================  crop the original rectangular image to a square ============================================================
     # original image:
@@ -50,14 +48,14 @@ while (True):
 
     resizedFrame = cv.cvtColor(resizedFrame, cv.COLOR_BGR2GRAY)
 
-    resizedFrame = (resizedFrame // len(asciiArr))
-
-    asciiFrame = np.zeros((originalX, originalX), dtype=np.uint8)
+    # resizedFrame = (resizedFrame / 255) * (len(asciiArr) + 1)
+    # resizedFrame = resizedFrame // len(asciiArr) * len(asciiArr)
+    # resizedFrame = [  [ int(i) for i in j ] for j in resizedFrame]
 
     # ======================================================== assign respective ascii values to the frame ==================================================
     # using nested loops, which is a horrible way to do it
 
-    # some weird roundabout way to get the font to reder based on pixel size:
+    # ------  some weird roundabout way to get the font to reder based on pixel size -----------------
     # calculating a factor here
     fontScale = 10
     fontFace = cv.FONT_HERSHEY_PLAIN
@@ -66,26 +64,26 @@ while (True):
         "", fontFace=fontFace, fontScale=fontScale, thickness=1)  # empty string is good enough
     factor = (fh-1) / fontScale
 
-    # using the factor now
     thickness = 1
-    height_in_pixels = 4  # or 20, code works either way
+    height_in_pixels = asciiWidth  # or 20, code works either way
     fontScale = (height_in_pixels - thickness) / factor
+
+    # =================================================================== main conversion ====================================================================
+
+    asciiFrame = np.zeros((originalX, originalX), dtype=np.uint8)
 
     for i in range(0, originalX, asciiWidth):
         for j in range(0, originalX, asciiWidth):
 
             pixel = resizedFrame[i // asciiWidth, j // asciiWidth]
-            # print(pixel)
 
-            asciiChar = asciiArr[pixel % len(asciiArr)]
+            asciiChar = asciiArr[int(pixel / 256 * len(asciiArr))]
 
             asciiFrame = cv.putText(asciiFrame, asciiChar, (j, i), fontFace=fontFace,
                                     fontScale=fontScale, color=255, thickness=thickness, bottomLeftOrigin=False)
 
-    # asciiFrame = cv.rotate(asciiFrame, cv.ROTATE_90_CLOCKWISE)
-
     cv.imshow("art", asciiFrame)
-    cv.imshow("original", resizedFrame * 20)
+    cv.imshow("original", (resizedFrame // len(asciiArr)))
 
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
