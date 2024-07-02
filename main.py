bda758bf5691bee816cf038a58cac667bdf98774
TheInvincible95 <109205375+TheInvincible95@users.noreply.github.com>
2024-07-02 14:51:39 +0530

    Changing quantizations to np vector operation

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 from math import floor
 
 
-path = "./rick.mp4"
+path = "./sampleVid.mp4"
 
 # comment the below line to use a video file, with the path specified above
 # path = 0
@@ -11,13 +11,14 @@ path = "./rick.mp4"
 vid = cv.VideoCapture(path)
 
 # resolution to represent each character by, in pixels
-asciiWidth = 4
+asciiWidth = 15
 
 # ascii array to store the mapping
 asciiArr = [' ', '.', '-', "'", ':', '_', ',', '^', '=', ';', '>', '<', '+', '!', 'r', 'c', '*', '/', 'z', '?', 's', 'L', 'T', 'v', ')', 'J', '7',
             '(', '|', 'F', 'i', '{', 'C', '}', 'f', 'I', '3', '1', 't', 'l', 'u', '[', 'n', 'e', 'o', 'Z', '5', 'Y', 'x', 'j', 'y', 'a', ']', '2',
             'E', 'S', 'w', 'q', 'k', 'P', '6', 'h', '9', 'd', '4', 'V', 'p', 'O', 'G', 'b', 'U', 'A', 'K', 'X', 'H', 'm', '8', 'R', 'D', '#', '$',
             'B', 'g', '0', 'M', 'N', 'W', 'Q', '%', '&', '@']
+asciiLevels = len(asciiArr)
 
 while (True):
 
@@ -61,7 +62,7 @@ while (True):
 
     # ----------------  some weird roundabout way to get the font to render based on pixel size -----------------
     # calculating a factor here
-    fontScale = 10
+    fontScale = asciiWidth*32
     fontFace = cv.FONT_HERSHEY_PLAIN
 
     ((fw, fh), baseline) = cv.getTextSize(
@@ -75,14 +76,17 @@ while (True):
     # =================================================================== main conversion ====================================================================
 
     asciiFrame = np.zeros((originalX, originalX, 3), dtype=np.uint8)
-
+    grayFrame = np.rint(np.multiply(grayFrame, asciiLevels/256)).astype(int)
+     
+    
+    
     for i in range(0, originalX, asciiWidth):
         for j in range(0, originalX, asciiWidth):
 
             pixel = grayFrame[i // asciiWidth, j // asciiWidth]
 
             # map the intensity value to an ascii character
-            asciiChar = asciiArr[int(pixel / 256 * len(asciiArr))]
+            asciiChar = asciiArr[pixel]
 
             # find the colour of the original image, to set the ascii character to that colour
             b, g, r = resizedFrame[i // asciiWidth, j // asciiWidth]
